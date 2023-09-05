07f79d633fabb26514cbb44eb064fad9115485cf
Trevaz <124297981+Onlytrevaz@users.noreply.github.com>
2023-09-05 15:35:59 -0300

    nova atualizaçao, tentando de novo

--- modulo10.js
+++ modulo10.js
@@ -1,24 +1,6 @@
 <script>
-$(document).ready(function() {
-$('#cpf').mask('000.000.000-00');
-$('#cep').mask('00000-000');
-});
+$(document).ready(function(){
 
-$(document).ready(function() {
-$('#cpf').mask('000.000.000-00');
-$('#cep').mask('00000-000');
-
-$('#cadastro-form').submit(function(event) {
-event.preventDefault();
-
-var nome = $('#nome').val();
-var email = $('#email').val();
-var telefone = $('#telefone').val();
-var cpf = $('#cpf').val();
-var endereco = $('#endereco').val();
-var cep = $('#cep').val();
-
-console.log(nome, email, telefone, cpf, endereco, cep);
-});
+$("cpf").mask("000.000.000-00")
 });
 </script>
