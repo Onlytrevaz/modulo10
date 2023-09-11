cb04e853a0443ccf9a91288e96cef564675e3472
Trevaz <124297981+Onlytrevaz@users.noreply.github.com>
2023-09-11 19:59:43 -0300

    exercicio pronto
    exercicio pronto

--- modulo10.js
+++ modulo10.js
@@ -1,6 +1,12 @@
-<script>
 $(document).ready(function(){
-
-$("cpf").mask("000.000.000-00")
+$("#cpf").mask("000.000.000-00")
 });
-</script>
+$(document).ready(function(){
+$("#telefone").mask("(00) 00000-0000")
+});
+$(document).ready(function(){
+$("#cep").mask("00000-000")
+});
+
+
+
